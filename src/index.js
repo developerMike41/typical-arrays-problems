75cf880b4a1acf40f6d246b131bab155c3bfab31
developerMike41 <hello1mmike41@gmail.com>
2021-01-24 19:13:51 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  array.sort().reverse();
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  array.sort();
+  return array[0];
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  for (let i=0; i<array.length; i++) {
+    sum += array[i];
+  }
+  return sum / 2;
 }
