5c384a8237b943c8473846471e90eba8ccb61c54
developerMike41 <hello1mmike41@gmail.com>
2021-01-24 20:04:05 +0300

    fix: task solved

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 
 exports.min = function min (array) {
-  if (typeof array === "undefined") {
+  if (typeof array === 'undefined') {
     return 0;
+  }
+  else {
+    if(array.length == 0){
+      return 0;
+    }
   }
   return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  if (typeof array === "undefined") {
+  if (typeof array === 'undefined') {
     return 0;
+  }
+  else {
+    if(array.length == 0){
+      return 0;
+    }
   }
   return Math.max(...array);
 
 }
 
 exports.avg = function avg (array) {
-  if (typeof array === "undefined") {
+  if (typeof array === 'undefined') {
     return 0;
+  }
+  else {
+    if(array.length == 0){
+      return 0;
+    }
   }
   let sum = 0;
   for (let i=0; i < array.length; i++) {
